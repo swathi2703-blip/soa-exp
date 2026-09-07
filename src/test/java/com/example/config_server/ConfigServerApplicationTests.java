1ce8cc0fc058def82ea07f1e90731525ced276b9
2410030248-Swathi <2410030248@klh.edu.in>
2026-09-07 11:08:31 +0530

    Add centralized company microservice configurations

--- src/test/java/com/example/config_server/ConfigServerApplicationTests.java
+++ src/test/java/com/example/config_server/ConfigServerApplicationTests.java
@@ -2,12 +2,32 @@ package com.example.config_server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.config.environment.Environment;
+import org.springframework.cloud.config.server.environment.EnvironmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class ConfigServerApplicationTests {
 
+	@Autowired
+	private EnvironmentRepository environmentRepository;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void servesEmployeeServiceConfiguration() {
+		Environment environment = environmentRepository.findOne("employee-service", "default", null);
+
+		assertThat(environment.getPropertySources())
+			.anySatisfy(source -> {
+				assertThat(source.getSource().get("server.port")).isEqualTo("8081");
+				assertThat(source.getSource().get("spring.datasource.url"))
+					.isEqualTo("jdbc:mysql://localhost:3306/employee_db");
+			});
 	}
 
 }
